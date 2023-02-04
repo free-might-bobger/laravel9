2d1292b4e662dba77f2c6e3c0d27742d37f02da3
Bobby Gerez <bobby.gerez@monstar-lab.com>
2023-02-05 07:35:44 +0800

    Updates

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-(php artisan down) || true
+#(php artisan down) || true
 
 
 # Pull the latest version of the app
@@ -15,27 +15,27 @@ git pull origin main
 
 
 # Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+#composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 
 # Clear the old cache
-php artisan clear-compiled
+#php artisan clear-compiled
 
 
 # Recreate cache
-php artisan optimize
+#php artisan optimize
 
 
 # Compile npm assets
-npm run prod
+#npm run prod
 
 
 # Run database migrations
-php artisan migrate --force
+#php artisan migrate --force
 
 
 # Exit maintenance mode
-php artisan up
+#php artisan up
 
 
 echo "Deployment finished!"
