0af8403364da34132b77b543cfc97f054785fc15
Bobby Gerez <bobby.gerez@monstar-lab.com>
2023-02-05 16:45:32 +0800

    Updates

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+ 
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+
+        $this->assertTrue(true);
+    }
+}
