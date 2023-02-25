b4947cab54c0161e76888743057014d86c0cbd27
Bobby Gerez <bobby.gerez@monstar-lab.com>
2023-02-25 12:22:40 +0800

    Updates

--- resources/views/imageUpload.blade.php
+++ resources/views/imageUpload.blade.php
@@ -31,7 +31,7 @@
             </div>
         @endif
     
-        <form action="{{ route('image.upload.post') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ secure_url('image.upload.post') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="row">
     
